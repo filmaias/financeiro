3ce293bd596988cca840d7e764cdf6c152168770
filmaias <filmaias>
2018-02-21 15:29:41 -0300

    programa final rodando

--- financeiro.py
+++ financeiro.py
@@ -1,25 +1,32 @@
-def calcula_inss(salario):
-    if salario <= 1000.0:
+def calcula_inss(a):
+    if a <= 1000.0:
         return 0.0
-    elif salario <= 2000.0:
-        return salario*0.1
+    elif a <= 2000.0:
+        return a*0.1
     else:
-        return salario*0.2
-
-def calcula_ir(salario_base):
-    if salario_base <= 1400:
-        return 0.0
-    elif salario_base <= 2500.0:
-        return salario_base*0.12
-    elif salario <= 5000.0:
-        return salario_base*0.2
-    else:
-        salario*0.275
+        return a*0.2
 
 def desconto_dep(dep):
     return 100.0*dep
 
-    
+def calcula_ir(salario,dep):
+    salario_base=salario-calcula_inss(salario)-desconto_dep(dep)
+    if salario_base <= 1400:
+        return 0.0
+    elif salario_base <= 2500.0:
+        return salario_base*0.12
+    elif salario_base <= 5000.0:
+        return salario_base*0.2
+    else:
+        return salario_base*0.275
+
 
 def salario_liquido(salario,dep):
     return salario - calcula_inss(salario) - calcula_ir(salario,dep)
+
+a=float(input("Salário bruto: "))
+dep=int(input("Qtd de dependentes: "))
+
+final=salario_liquido(a,dep)
+
+print ("O salário liquído é de %.1f reais" %final )
