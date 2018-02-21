58f8b842c8a40b7ea2fa36e69c1c1055f385f54e
filmaias <filmaias>
2018-02-21 15:02:27 -0300

    programa completo

--- financeiro.py
+++ financeiro.py
@@ -1,5 +1,13 @@
-def inss(salario):
-    return salario*0.2
+def calcula_inss(salario):
+    if salario <= 1000:
+        return 0.0
+    elif salario <= 2000:
+        return salario*0.1
+    else:
+        return salario*0.2
 
-def ir(salario,dep):
-    return salario-(100*dep)-inss(salario)
+def calcula_ir(salario,dep):
+    return (salario-(100*dep)-inss(salario))*0.05
+
+def salario_liquido(salario,dep):
+    return salario - calcula_inss(salario) - calcula_ir(salario,dep)
